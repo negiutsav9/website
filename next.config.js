a4157496455ca57e8f337accd64de75e53e71872
negiutsav9 <utsavnegi12@gmail.com>
2024-05-12 00:47:51 -0400

    fix configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+  basePath: "/app",
+  images: {
+    domains: ['firebasestorage.googleapis.com']
+  },
+}
 
-module.exports = {
-    images: {
-      domains: ['firebasestorage.googleapis.com']
-    },
-  }
+module.exports = nextConfig
