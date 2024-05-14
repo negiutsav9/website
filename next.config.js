ac4cfe5a22032cd8805760c2a4f06398ed76b445
negiutsav9 <utsavnegi12@gmail.com>
2024-05-14 01:43:28 -0400

    Changed config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  basePath: "/app",
   images: {
-    domains: ['firebasestorage.googleapis.com']
-  },
+    domains: ['firebasestorage.googleapis.com'],
+  }
 }
 
 module.exports = nextConfig
